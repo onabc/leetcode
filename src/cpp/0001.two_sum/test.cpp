be9aacb47cb49d4dfa74c9d36fb350f791b014ba
DaboQ <hesbiu@gmail.com>
2021-11-19 17:43:26 +0800

    feat(*)

--- src/cpp/0001.two_sum/test.cpp
+++ src/cpp/0001.two_sum/test.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "two_sum.h"
+#include <unordered_map>
 
 using namespace std;
 
--- src/cpp/0001.two_sum/two_sum.cpp
+++ src/cpp/0001.two_sum/two_sum.cpp
@@ -1,9 +1,26 @@
 #include <vector>
 
 #include "two_sum.h"
+#include <unordered_map>
 
 using namespace std;
 
+vector<int> twoSum(vector<int>& nums, int target) 
+{
+    unordered_map<int, int> map;
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        auto it = map.find(target - nums[i]);
+        if (it != map.end()) {
+            return { it->second, i };
+        }
+        map[nums[i]] = i;
+    }
+    return {};
+}
+
+/*
 vector<int> twoSum(vector<int>& nums, int target) {
     int n = nums.size();
     for (int i = 0; i < n; ++i) {
@@ -15,3 +32,4 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return {};
 }
+*/
