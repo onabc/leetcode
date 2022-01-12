c8ff3a9b954204a22cbe760b2a1c6c2d3f8800ee
DaboQ <hesbiu@gmail.com>
2022-01-12 11:27:37 +0800

    feat(0034):Find First and Last Position of Element in Sorted Array

---
+++ 0034.Find First and Last Position of Element in Sorted Array/TEST.cc
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_MAIN
+#include "../Catch/catch.hpp"
+#include "solution.h"
+
+TEST_CASE("Search in Rotated Sorted Array", "[search]")
+{
+	Solution s;
+	SECTION("common")
+	{
+		vector<int> arr{ 5, 7, 7, 8, 8, 10 };
+		REQUIRE(s.searchRange(arr, 8) == vector<int>{ 3, 4 });
+	}
+
+	SECTION("no")
+	{
+		vector<int> arr{ 1 };
+		REQUIRE(s.searchRange(arr, 0) == vector<int>{ -1, -1 });
+	}
+
+	SECTION("all")
+	{
+		vector<int> arr{ 2,2 };
+		REQUIRE(s.searchRange(arr, 2) == vector<int>{ 0, 1 });
+	}
+}
