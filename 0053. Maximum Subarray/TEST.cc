33d54ee0c53be25c3aeb4ec8efcd57cbe1e38bce
daboq <hesbiu@gmail.com>
2023-03-01 17:39:10 +0800

    feat(0053. Maximum Subarray)

---
+++ 0053. Maximum Subarray/TEST.cc
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "../Catch/catch.hpp"
+#include "solution.h"
+
+TEST_CASE("Maximum Subarray", "[maxSubArray]")
+{
+	Solution solution;
+
+	SECTION("one")
+	{
+		vector<int> nums{ -2,1,-3,4,-1,2,1,-5,4 };
+		REQUIRE(solution.maxSubArray(nums) == 6);
+	}
+
+	SECTION("two")
+	{
+		vector<int> nums{ 5,4,-1,7,8 };
+		REQUIRE(solution.maxSubArray(nums) == 23);
+	}
+}
--- 0070.Climbing Stairs/solution.h
+++ 0070.Climbing Stairs/solution.h
@@ -14,11 +14,10 @@ public:
 	}*/
 
 	int climbStairs(int n) {
-		int n1 = 0, n2 = 1;
-		for (int i = 1; i <= n; ++i)
-		{
-			int tmp = n2;
-			n2 = n2 + n1;
+		int n1 = 0, n2 = 1, tmp;
+		for (int i = 1; i <= n; ++i) {
+			tmp = n2;
+			n2 += n1;
 			n1 = tmp;
 		}
 		return n2;
