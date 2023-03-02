380d458ab55eaa8aabf540b952d84b3c2d0d5d52
daboq <hesbiu@gmail.com>
2023-03-02 17:55:47 +0800

    refactor(0056.Merge Intervals)

--- 0056.Merge Intervals/solution.h
+++ 0056.Merge Intervals/solution.h
@@ -3,20 +3,36 @@ using namespace std;
 class Solution {
  public:
   vector<vector<int>> merge(vector<vector<int>> &intervals) {
-	vector<vector<int>> ans;
-	// 排序后保证每次提交的合并区间都不会有缺漏
 	sort(intervals.begin(), intervals.end());
-	// 当前待合并区间的开始和结束
-	int start = intervals[0][0], end = intervals[0][1];
-	for (auto item : intervals) {
-	  if (item[0] > end) {
-		ans.push_back({start, end});
-		start = item[0];
+	vector<vector<int>> ans;
+	for (int i = 0; i < intervals.size();) {
+	  int r = intervals[i][1];
+	  int j = i + 1;
+	  while (j < intervals.size() && r >= intervals[j][0]) {
+		r = max(r, intervals[j][1]);
+		++j;
 	  }
-	  end = max(end, item[1]);
+	  ans.push_back({intervals[i][0], r});
+	  i = j;
 	}
-	// 把最后一个区间提交上去
-	ans.push_back({start, end});
 	return ans;
   }
+//  vector<vector<int>> merge(vector<vector<int>> &intervals) {
+//	// 排序后保证每次提交的合并区间都不会有缺漏
+//	sort(intervals.begin(), intervals.end());
+//	vector<vector<int>> res;
+//	// 当前待合并区间的开始和结束
+//	int start = intervals[0][0], end = intervals[0][1];
+//	for (auto item : intervals) {
+//	  // 当前区间不能再合并了，需要提交
+//	  if (item[0] > end) {
+//		res.push_back({start, end});
+//		start = item[0];
+//	  }
+//	  end = max(end, item[1]);
+//	}
+//	// 把最后一个区间提交上去
+//	res.push_back({start, end});
+//	return res;
+//  }
 };
